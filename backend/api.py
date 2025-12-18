fb11a2962848eedeb131202eb3781c547f19b89c
ChinmayNakwa <chinmaynakwa29012005@gmail.com>
2025-12-19 03:08:50 +0530

    fix: path in api.py

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.dirname(__file__)) 
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware 
